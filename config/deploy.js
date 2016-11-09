7f1a0fff6b5519326a4090ec810014dcffe77c2a
Andy Richardson <andy@kohactive.com>
2016-11-09 13:19:44 -0500

    Deployment

---
+++ config/deploy.js
@@ -0,0 +1,45 @@
+/* jshint node: true */
+
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {}
+    // include other plugin configuration that applies to all deploy targets here
+  };
+
+  ENV.s3 = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    bucket: process.env.AWS_BUCKET,
+    region: process.env.AWS_REGION,
+    prefix: process.env.AWS_DIRECTORY
+  };
+
+  ENV['s3-index'] = {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    bucket: process.env.AWS_BUCKET,
+    region: process.env.AWS_REGION,
+    prefix: process.env.AWS_DIRECTORY,
+    allowOverwrite: true
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
